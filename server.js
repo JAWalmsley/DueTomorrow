9799f2e62ef30714d120514429ab23ae411aced8
Jack Walmsley <jackawalmsley@gmail.com>
2022-04-05 19:35:33 -0400

    Checkboxes work with POST

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const bodyparser = require('body-parser')
+
 const app = express()
+app.use(bodyparser.json())
 const port = 3000
 
 app.set('views', './views')
@@ -10,8 +13,13 @@ app.use('/css', express.static(__dirname + '/css'))
 app.use('/js', express.static(__dirname + '/js'))
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', {assignments: [['one', 'two', 'three', false], ['ones', 'two', 'three', false], ['oness', 'two', 'three', true], ['onesss', 'two', 'three', true]]})
 })
+
+app.post('/complete', (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200)
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
