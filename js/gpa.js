8e3c608a6d9677906da62281921a978b525cceb1
Jack Walmsley <jackawalmsley@gmail.com>
2022-06-06 01:10:39 -0400

    Added GPA systems 12 pt and 4 pt

--- js/gpa.js
+++ js/gpa.js
@@ -7,7 +7,47 @@ function weightedAverage(items, weights) {
     return weightedTotal / weightSum;
 }
 
+function GPAScale(grade, scale) {
+    let GPA = 0;
+    for (let [percent, number] of Object.entries(scale)) {
+        if(grade >= percent) {
+            GPA = number;
+        }
+    };
+    return GPA;
+}
+
 function fillGrades() {
+    const twelvePointScale = {
+        0:0,
+        50:1,
+        53:2,
+        57:3,
+        60:4,
+        63:5,
+        67:6,
+        70:7,
+        73:8,
+        77:9,
+        80:10,
+        85:11,
+        90:12
+    }
+    const fourPointScale = {
+        0:0,
+        50:0.7,
+        53:1,
+        57:1.3,
+        60:1.7,
+        63:2,
+        67:2.3,
+        70:2.7,
+        73:3,
+        77:3.3,
+        80:3.7,
+        85:4,
+        90:4.3
+    }
     let grades = [];
     let credits = [];
     $(".average").each(function() {
@@ -21,7 +61,10 @@ function fillGrades() {
         credits.push(course.credits);
         $(this).closest("tr").find(".average").text(avg.toPrecision(4) + "%");
     });
-    $("#total").text(weightedAverage(grades, credits).toPrecision(4) + "%");
+    let gpaPercent = weightedAverage(grades, credits).toPrecision(4)
+    $("#total").text(gpaPercent + "%");
+    $("#12pt").text(GPAScale(gpaPercent, twelvePointScale));
+    $("#4pt").text(GPAScale(gpaPercent, fourPointScale));
 }
 
 $(document).ready(function () {
@@ -51,6 +94,5 @@ $(document).ready(function () {
                 fillGrades();
             })
         }
-        
     });
 });
