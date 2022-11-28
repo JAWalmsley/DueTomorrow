61c4407c1432734e6b0bdcd503060f304027be15
Jack Walmsley <jackawalmsley@gmail.com>
2022-11-28 03:12:16 -0500

    Total GPA card

--- bte_react/src/components/gpa.js
+++ bte_react/src/components/gpa.js
@@ -1,3 +1,14 @@
+import {
+    Card,
+    CardContent,
+    Grid,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    TextField,
+} from '@mui/material';
 import React from 'react';
 
 /**
@@ -6,47 +17,23 @@ import React from 'react';
  * @param {Object} assignment
  */
 export class GradeRow extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { value: '' };
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
-
     render() {
         const assignment = this.props.assignment;
         const { id, name, due, weight, done, grade } = assignment;
-        this.state.value = grade;
         let gradeCell = <td></td>;
         if (done) {
             gradeCell = (
-                <td className="center">
-                    <div className="input-field">
-                        <input
-                            className="grade"
-                            type="text"
-                            value={this.state.value}
-                            onChange={this.handleChange}
-                            style={{
-                                height: '90%',
-                                margin: '0px',
-                                width: '100%',
-                            }}
-                        />
-                    </div>
-                </td>
+                <TableCell className="center">
+                    <TextField size="small" label="Grade" value={grade} variant="standard"/>
+                </TableCell>
             );
         }
         return (
-            <tr key={id}>
-                <td>{name}</td>
-                <td className="center">{weight}</td>
+            <TableRow key={id}>
+                <TableCell>{name}</TableCell>
+                <TableCell>{weight}</TableCell>
                 {gradeCell}
-            </tr>
+            </TableRow>
         );
     }
 }
@@ -59,11 +46,15 @@ export class GradeRow extends React.Component {
 export class CourseHeader extends React.Component {
     render() {
         return (
-            <tr>
-                <td className="center">{this.props.course.name}</td>
-                <td className="center">{this.props.course.credits} Credits</td>
-                <td className="center">40%</td>
-            </tr>
+            <TableRow>
+                <TableCell className="center">
+                    {this.props.course.name}
+                </TableCell>
+                <TableCell className="center">
+                    {this.props.course.credits} Credits
+                </TableCell>
+                <TableCell className="center">40%</TableCell>
+            </TableRow>
         );
     }
 }
@@ -78,19 +69,19 @@ export class HeaderRow extends React.Component {
         const cells = [];
         this.props.columns.forEach((column) => {
             cells.push(
-                <td key={column} className="center-align">
+                <TableCell key={column} className="center-align">
                     <b>{column}</b>
-                </td>
+                </TableCell>
             );
         });
-        return <tr>{cells}</tr>;
+        return <TableRow>{cells}</TableRow>;
     }
 }
 
 /**
  * Box containing all assignments for a course
  *
- * @param {Object} assignment
+ * @param {Object} assignments
  * @param {Object} course
  */
 export class CourseBox extends React.Component {
@@ -102,16 +93,108 @@ export class CourseBox extends React.Component {
             );
         });
         return (
-            <table className="highlight col s6" id={this.props.course.id}>
-                <thead style={{ backgroundColor: this.props.course.colour }}>
-                    <CourseHeader course={this.props.course} />
-                    <HeaderRow
-                        columns={['Assignment', 'Weight', 'Grade']}
-                        colour={this.props.course.colour}
-                    />
-                </thead>
-                <tbody>{rows}</tbody>
-            </table>
+            <Card>
+                <CardContent>
+                    <Table id={this.props.course.id}>
+                        <TableHead
+                            style={{
+                                backgroundColor: this.props.course.colour,
+                            }}
+                        >
+                            <CourseHeader course={this.props.course} />
+                            <HeaderRow
+                                columns={['Assignment', 'Weight', 'Grade']}
+                                colour={this.props.course.colour}
+                            />
+                        </TableHead>
+                        <TableBody>{rows}</TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
+        );
+    }
+}
+
+/**
+ * The tally of total GPA
+ * @param courses
+ */
+export class GPATally extends React.Component {
+    render() {
+        const twelvePointScale = {
+            0:0,
+            50:1,
+            53:2,
+            57:3,
+            60:4,
+            63:5,
+            67:6,
+            70:7,
+            73:8,
+            77:9,
+            80:10,
+            85:11,
+            90:12
+        }
+        const fourPointScale = {
+            0:0,
+            50:0.7,
+            53:1,
+            57:1.3,
+            60:1.7,
+            63:2,
+            67:2.3,
+            70:2.7,
+            73:3,
+            77:3.3,
+            80:3.7,
+            85:4,
+            90:4.3
+        }
+
+        function weightedAverage(items, weights) {
+            let weightSum = weights.reduce((partialSum, a) => partialSum + a, 0);
+            let weightedTotal = 0
+            for(let i = 0; i < weights.length; i++) {
+                weightedTotal += weights[i] * items[i];
+            }
+            return weightedTotal / weightSum;
+        }
+        
+        function GPAScale(grade, scale) {
+            let GPA = 0;
+            for (let [percent, number] of Object.entries(scale)) {
+                if(grade >= percent) {
+                    GPA = number;
+                }
+            };
+            return GPA;
+        }
+        let rows = [];
+        this.props.courses.forEach(function(course) {
+            let avg = weightedAverage(course.assignments.map(a => a.grade), course.assignments.map(a => a.weight));
+            let fourpt = GPAScale(avg, fourPointScale);
+            let twelvept = GPAScale(avg, twelvePointScale);
+            rows.push(<TableRow>
+                <TableCell>{course.name}</TableCell>
+                <TableCell>{course.credits}</TableCell>
+                <TableCell>{twelvept}</TableCell>
+                <TableCell>{fourpt}</TableCell>
+            </TableRow>)
+        });
+        return (
+            <Card>
+                <CardContent>
+                    <Table>
+                        <TableHead>
+                            <HeaderRow columns={['Course', 'Credits', '12pt', '4pt']} colour="#fff"/>
+                        </TableHead>
+                        <TableBody>
+                            {rows}
+                        </TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
         );
     }
 }
@@ -126,9 +209,14 @@ export class CourseList extends React.Component {
         const boxes = [];
         this.props.courses.forEach((course) => {
             boxes.push(
-                <CourseBox assignments={course.assignments} course={course} />
+                <Grid item>
+                    <CourseBox
+                        assignments={course.assignments}
+                        course={course}
+                    />
+                    </Grid>
             );
         });
-        return <div>{boxes}</div>;
+        return <Grid container spacing={2}>{boxes}</Grid>;
     }
 }
--- bte_react/src/routes/gpaPage.js
+++ bte_react/src/routes/gpaPage.js
@@ -1,4 +1,5 @@
-import { CourseList } from '../components/gpa.js';
+import { Container, Grid } from '@mui/material';
+import { CourseList, GPATally } from '../components/gpa.js';
 import { Navbar } from '../components/navbar.js';
 
 import '../css/styles.css';
@@ -11,38 +12,41 @@ const ASSIGNMENTS = [
         due: '2003-09-30T04:00:00.000Z',
         done: 1,
         weight: 50,
-        grade: 60,
+        grade: 100,
     },
 ];
 const COURSES = [
     {
-        "name": "Balls",
-        "colour": '#ffff00',
-        "assignments": ASSIGNMENTS,
-        "id": 55389202839382,
-        "credits": 3
+        name: 'Balls',
+        colour: '#ffffaa',
+        assignments: ASSIGNMENTS,
+        id: 55389202839382,
+        credits: 3,
     },
     {
-        "name": "Balls Course 2",
-        "colour": "#0000ff",
-        "assignments": ASSIGNMENTS,
-        "id": 92821201821,
-        "credits": 6
-    }
-]
+        name: 'Balls Course 2',
+        colour: '#aaaaff',
+        assignments: ASSIGNMENTS,
+        id: 92821201821,
+        credits: 6,
+    },
+];
 const USERNAME = 'Jack';
 
 export default function GPAPage() {
     return (
         <>
             <Navbar username={USERNAME} />
-            <div className="container">
-                <div className="row">
-                    <div className="col s12">
+            <Container>
+                <Grid container spacing={2} padding={1}>
+                    <Grid item xs={7}>
                         <CourseList courses={COURSES} />
-                    </div>
-                </div>
-            </div>
+                    </Grid>
+                    <Grid item xs={5} spacing={2}>
+                        <GPATally courses={COURSES}/>
+                    </Grid>
+                </Grid>
+            </Container>
         </>
     );
 }
