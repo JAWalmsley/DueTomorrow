672d373a480e163c755184b89f16d42925647f8c
Jack Walmsley <jackawalmsley@gmail.com>
2022-12-03 17:50:20 -0500

    Login Page

---
+++ apps/ui/src/routes/loginPage.js
@@ -0,0 +1,13 @@
+import { Container} from '@mui/material';
+import { LoginBox } from '../components/login';
+
+
+export default function LoginPage() {
+    return (
+        <>
+            <Container>
+                <LoginBox />
+            </Container>
+        </>
+    );
+}
