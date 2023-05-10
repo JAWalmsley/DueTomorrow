91e7537ffd155db9b0efce48b9efdede2b560ff7
JAWalmsley <jackawalmsley@gmail.com>
2023-05-10 00:54:42 +0000

    Docker setup on server

--- apps/api/dbManager.js
+++ apps/api/dbManager.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 let con = mysql.createPool({
     connectionLimit: 10,
-    host: 'host.docker.internal',
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
