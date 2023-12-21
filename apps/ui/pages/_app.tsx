4d072229ff568913e0d35d081924afa8c08f6e9e
Jack Walmsley <jackawalmsley@gmail.com>
2023-12-21 01:07:19 -0500

    Using workbox for pwa but not working?

--- apps/ui/pages/_app.tsx
+++ apps/ui/pages/_app.tsx
@@ -1,15 +1,15 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
+import { Workbox } from 'workbox-window';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-
-  React.useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then((registration) => console.log('scope is: ', registration.scope));
-    }
-  }, []);
-  
-  return <Component {...pageProps} />
+    const isProduction = process.env.NODE_ENV === 'production';
+    React.useEffect(() => {
+        if (isProduction && 'serviceWorker' in navigator) {
+            // check your path, this is just an example
+            const wb = new Workbox('../../../../service-worker.js');
+            wb.register();
+        }
+    }, []);
+    return <Component {...pageProps} />;
 }
