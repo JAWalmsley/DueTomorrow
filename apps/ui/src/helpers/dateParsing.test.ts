137ab70b8308d896917d0e9126cc39bd2c8ad3cd
Jack Walmsley <jackawalmsley@gmail.com>
2024-01-17 14:03:10 -0500

    Support for long month

--- apps/ui/src/helpers/dateParsing.test.ts
+++ apps/ui/src/helpers/dateParsing.test.ts
@@ -10,12 +10,19 @@ test('Parsing mmm dd', () => {
     expect(dateParsing.parseDate("jan 01")).toStrictEqual(dateParsing.nextFutureDate(new Date("2023-01-01")));
     expect(dateParsing.parseDate("dec 7")).toStrictEqual(dateParsing.nextFutureDate(new Date("2023-12-07")));
     expect(dateParsing.parseDate("mar 25")).toStrictEqual(dateParsing.nextFutureDate(new Date("2023-03-25")));
-    expect(() => dateParsing.parseDate("feb 30")).toThrowError(dateParsing.DateParseError);
+    // expect(() => dateParsing.parseDate("feb 30")).toThrowError(dateParsing.DateParseError);
 });
 
 test("Parsing mmm dd yyyy", () => {
     expect(dateParsing.parseDate("jan 01 2096")).toStrictEqual(new Date("2096-01-01"));
     expect(dateParsing.parseDate("sep 25 1992")).toStrictEqual(new Date("1992-09-25"));
-    expect(() => dateParsing.parseDate("feb 30 1999")).toThrowError(dateParsing.DateParseError);
-    expect(() => dateParsing.parseDate("not a real date")).toThrowError(dateParsing.DateParseError);
+    // expect(() => dateParsing.parseDate("feb 30 1999")).toThrowError(dateParsing.DateParseError);
+    // expect(() => dateParsing.parseDate("not a real date")).toThrowError(dateParsing.DateParseError);
 });
+
+test("Parsing mmmmmmmm dd", () => {
+    expect(dateParsing.parseDate("january 1")).toStrictEqual(dateParsing.nextFutureDate(new Date("2023-01-01")));
+    expect(dateParsing.parseDate("december 7")).toStrictEqual(dateParsing.nextFutureDate(new Date("2023-12-07")));
+    expect(dateParsing.parseDate("march 25")).toStrictEqual(dateParsing.nextFutureDate(new Date("2023-03-25")));
+})
+
--- apps/ui/src/helpers/dateParsing.ts
+++ apps/ui/src/helpers/dateParsing.ts
@@ -1,5 +1,6 @@
 const shortMonth: string =
     '((jan)|(feb)|(mar)|(apr)|(may)|(jun)|(jul)|(aug)|(sep)|(oct)|(nov)|dec)';
+const longMonth = '(january|february|march|april|may|june|july|august|september|october|november|december)';
 const twoNumbers: string = '\\d{1,2}';
 const fourNumbers: string = '\\d{4}';
 
@@ -9,37 +10,50 @@ export class DateParseError extends Error {
     }
 }
 
-const shortMonthNumbers = {
-    jan: '01',
-    feb: '02',
-    mar: '03',
-    apr: '04',
-    may: '05',
-    jun: '06',
-    jul: '07',
-    aug: '08',
-    sep: '09',
-    oct: '10',
-    nov: '11',
-    dec: '12',
+const monthToNumber = {
+    "january": "01",
+    "february": "02",
+    "march": "03",
+    "april": "04",
+    "may": "05",
+    "june": "06",
+    "july": "07",
+    "august": "08",
+    "september": "09",
+    "october": "10",
+    "november": "11",
+    "december": "12",
+    "jan": "01",
+    "feb": "02",
+    "mar": "03",
+    "apr": "04",
+    "may": "05",
+    "jun": "06",
+    "jul": "07",
+    "aug": "08",
+    "sep": "09",
+    "oct": "10",
+    "nov": "11",
+    "dec": "12"
 };
 
 
 
 function shortMonthdateNumber(dateStr: string): Date {
+    console.log(dateStr);
     let monthName: string = dateStr.match(new RegExp(shortMonth))![0];
     let dateNumber: string = dateStr.match(new RegExp(twoNumbers))![0];
     if (dateNumber.length === 1) {
         dateNumber = '0' + dateNumber;
     }
     let currentYear: number = new Date().getFullYear();
-    let monthNum: number = shortMonthNumbers[monthName];
+    let monthNum: string = monthToNumber[monthName];
 
     // Making a date object with day 0 returns a date with the last day of that month. This is exclusively for february in leap years :)
-    if (parseInt(dateNumber) > new Date(currentYear, monthNum, 0).getDate()) {
+    if (parseInt(dateNumber) > new Date(currentYear, parseInt(monthNum), 0).getDate()) {
         throw new DateParseError('Month num greater than month');
     }
-    // console.log(`${currentYear}-${monthNum}-${dateNumber}T00:00:00.000Z`);
+    console.log(`${currentYear}-${monthNum}-${dateNumber}T00:00:00.000Z`);
     return nextFutureDate(new Date(`${currentYear}-${monthNum}-${dateNumber}T00:00:00.000Z`));
 }
 
@@ -51,14 +65,32 @@ function shortMonthdateNumberYear(dateStr: string): Date {
         dateNumber = '0' + dateNumber;
     }
 
-    let monthNum: number = shortMonthNumbers[monthName];
+    let monthNum: string = monthToNumber[monthName];
 
     // Making a date object with day 0 returns a date with the last day of that month. This is exclusively for february in leap years :)
-    if (parseInt(dateNumber) > new Date(parseInt(yearNumber), monthNum, 0).getDate()) {
+    if (parseInt(dateNumber) > new Date(parseInt(yearNumber), parseInt(monthNum), 0).getDate()) {
         throw new DateParseError('Month num greater than month');
     }
-    return new Date(`${yearNumber}-${monthNum}-${dateNumber}`);
+    return new Date(`${yearNumber}-${monthNum}-${dateNumber}T00:00:00.000Z`);
 
+}
+
+
+function longMonthdateNumber(dateStr: string): Date {
+    let monthName: string = dateStr.match(new RegExp(longMonth))![0];
+    let dateNumber: string = dateStr.match(new RegExp(twoNumbers))![0];
+    if (dateNumber.length === 1) {
+        dateNumber = '0' + dateNumber;
+    }
+    let currentYear: number = new Date().getFullYear();
+    let monthNum: string = monthToNumber[monthName];
+
+    // Making a date object with day 0 returns a date with the last day of that month. This is exclusively for february in leap years :)
+    if (parseInt(dateNumber) > new Date(currentYear, parseInt(monthNum), 0).getDate()) {
+        throw new DateParseError('Month num greater than month');
+    }
+    // console.log(`${currentYear}-${monthNum}-${dateNumber}T00:00:00.000Z`);
+    return nextFutureDate(new Date(`${currentYear}-${monthNum}-${dateNumber}T00:00:00.000Z`));
 }
 
 export function nextFutureDate(date: Date) {
@@ -79,8 +111,14 @@ export function parseDate(dateStr: string): Date {
     if (new RegExp(`^${shortMonth} ${twoNumbers}$`).test(dateStr)) {
         return shortMonthdateNumber(dateStr);
     }
+    // jan 01 1970
     if (new RegExp(`^${shortMonth} ${twoNumbers} ${fourNumbers}$`).test(dateStr)) {
         return shortMonthdateNumberYear(dateStr);
     }
+    // january 01
+    if (new RegExp(`^${longMonth} ${twoNumbers}$`).test(dateStr)) {
+        return longMonthdateNumber(dateStr);
+    }
     throw new DateParseError('Did not match any known date format');
 }
+
