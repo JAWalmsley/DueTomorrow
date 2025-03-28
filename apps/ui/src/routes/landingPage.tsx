f2a91b065d62342bb4dc2e657e0ec37911f0db5b
Jack Walmsley <jackawalmsley@gmail.com>
2025-03-27 23:43:10 -0400

    Decent start for landing page

---
+++ apps/ui/src/routes/landingPage.tsx
@@ -0,0 +1,56 @@
+import { CenterFocusStrong, Title } from '@mui/icons-material';
+import { AppBar, Box, Button, Container, Grid, Paper, styled, Toolbar, Typography } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2';
+import React from 'react'
+
+export default function LandingPage() {
+    return (
+        <>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography
+                            variant="h3"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            DT
+                        </Typography>
+                        <Button color="inherit" href="/login" >
+                            <Typography variant="h6">Log In</Typography>
+                        </Button>
+                        <Button color="inherit" href="/login" >
+                            <Typography variant="h6">Register</Typography>
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            <Container >
+                <Grid container paddingTop={10} alignItems="center">
+                    <Grid item xs={12}>
+                    <Typography align='center' variant="h3">Better than a spreadsheet.</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography align='center' variant="body1" color="GrayText">Track all your assignments in one place and get a notification when there's something you forgot.</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Box height="64px" />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Box component="img" src="assignmentspage.png" style={{maxWidth:'100%'}}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Box height="48px" />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography align='center' variant="h6" >"DueTomorrow saves me from failing a class at least once a year"</Typography>
+                        <Typography align='center' variant="body1" color="GrayText">Jack Walmsley, Founder & CEO</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography align='center' variant="h6" >"I used to write down all my assignments on my arm but then I couldn't shower until I finished all of them. I much prefer the workflow of DueTomorrow."</Typography>
+                        <Typography align='center' variant="body1" color="GrayText">Anonymous</Typography>
+                    </Grid>
+                </Grid>
+                
+            </Container>
+        </>
+    );
+}
